754f2d162ed5de4f7021d21c466a421a31ea9b34
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-01-31 22:41:11 -0500

    Added Skeleton Structure for Heuristic_Sequence

--- utils/models/heuristic_sequence.py
+++ utils/models/heuristic_sequence.py
@@ -4,4 +4,17 @@ import utils.abstract.stratagem
 class Heuristic_Sequence(Stratagem):
 
     def __init__(self):
+        super().__init__()
+        
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.traits = []
+
+    def train(self):
+        pass
+
+    def iterate(self):
+        pass
+
+    def operate(self):
         pass
